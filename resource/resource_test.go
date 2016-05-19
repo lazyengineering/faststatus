aa8a3122419d672bbb2d5d34993bdad44441157f
Jesse Allen <jesse@jessecarl.com>
2016-05-19 16:46:22 -0700

    Adds Resource Marshal Unmarshal JSON Test
    Adds test for Resource to check that Unmarshalling a Marshaled Resource
    results in an equivalent Resource or a zero value when an invalid Status
    is passed.

--- resource/resource_test.go
+++ resource/resource_test.go
@@ -260,3 +260,121 @@ func TestResourceUnmarshalJSON(t *testing.T) {
 		}
 	}
 }
+
+func TestResourceMarshalUnmarshalJSON(t *testing.T) {
+	type testResponse struct {
+		Value Resource
+		Err   error
+	}
+	type jsonTest struct {
+		Input    Resource
+		Expected testResponse
+	}
+	tests := []jsonTest{
+		jsonTest{ // Zero Value
+			Input: Resource{},
+			Expected: testResponse{
+				Resource{},
+				nil,
+			},
+		},
+		jsonTest{ // Valid Busy
+			Input: Resource{
+				Id:           1,
+				FriendlyName: "First One",
+				Status:       Busy,
+				Since: func() time.Time {
+					tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:25:00-07:00")
+					return tt
+				}(),
+			},
+			Expected: testResponse{
+				Resource{
+					Id:           1,
+					FriendlyName: "First One",
+					Status:       Busy,
+					Since: func() time.Time {
+						tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:25:00-07:00")
+						return tt
+					}(),
+				},
+				nil,
+			},
+		},
+		jsonTest{ // Valid Free
+			Input: Resource{
+				Id:           15,
+				FriendlyName: "Second One",
+				Status:       Free,
+				Since: func() time.Time {
+					tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:27:00-07:00")
+					return tt
+				}(),
+			},
+			Expected: testResponse{
+				Resource{
+					Id:           15,
+					FriendlyName: "Second One",
+					Status:       Free,
+					Since: func() time.Time {
+						tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:27:00-07:00")
+						return tt
+					}(),
+				},
+				nil,
+			},
+		},
+		jsonTest{ // Valid Occupied
+			Input: Resource{
+				Id:           175,
+				FriendlyName: "Third One",
+				Status:       Occupied,
+				Since: func() time.Time {
+					tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:28:00-07:00")
+					return tt
+				}(),
+			},
+			Expected: testResponse{
+				Resource{
+					Id:           175,
+					FriendlyName: "Third One",
+					Status:       Occupied,
+					Since: func() time.Time {
+						tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:28:00-07:00")
+						return tt
+					}(),
+				},
+				nil,
+			},
+		},
+		jsonTest{ // Out of Range
+			Input: Resource{
+				Id:           3503,
+				FriendlyName: "Another One",
+				Status:       Occupied + 1,
+				Since: func() time.Time {
+					tt, _ := time.Parse(time.RFC3339, "2016-05-12T16:30:00-07:00")
+					return tt
+				}(),
+			},
+			Expected: testResponse{
+				Resource{},
+				ErrOutOfRange,
+			},
+		},
+	}
+	for _, st := range tests {
+		actual, err := func(r Resource) (Resource, error) {
+			ac := new(Resource)
+			tmp, erx := json.Marshal(r)
+			if erx != nil {
+				return *ac, erx
+			}
+			erx = json.Unmarshal(tmp, ac)
+			return *ac, erx
+		}(st.Input)
+		if !rootError(err, st.Expected.Err) || !actual.Eq(st.Expected.Value) {
+			t.Errorf("\nexpected:\t%v\t%v\n  actual:\t%v\t%v", st.Expected.Value, st.Expected.Err, actual, err)
+		}
+	}
+}
